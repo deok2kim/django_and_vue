12bb221a1f07edeea990e3f6071018a75e6664a5
deokgikim <dukgi77@naver.com>
2020-04-27 12:12:10 +0900

    vscode test

---
+++ web_pjt3/community/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'community'
+
+urlpatterns = [
+    path('', views.review_list, name='review_list'),
+    path('create/', views.create, name='create'),
+    path('<int:review_pk>/', views.detail, name='detail'),
+
+
+]
