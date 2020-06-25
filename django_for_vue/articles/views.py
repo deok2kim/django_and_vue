ddfd70da7b5f393090a669cb7a2eee6275787dec
deokgikim <dukgi77@naver.com>
2020-06-25 13:58:49 +0900

    articles basic & accounts rest-auth login

--- django_for_vue/articles/views.py
+++ django_for_vue/articles/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Article
+from .serializers import ArticleListSerializer, ArticleSerializer
+
+@api_view(['GET'])
+def article_list(request):
+    articles = Article.objects.all()
+    serializer = ArticleListSerializer(articles, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def article_detail(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    serializer = ArticleSerializer(article)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_article(request):
+    serializer = ArticleSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data)
